51ee01f70361867db4e1db3f19dcc44555ebdd04
Maitrog <35650757+Maitrog@users.noreply.github.com>
2021-09-16 19:44:33 +0300

    Delete old field in settings

--- Six Screens Controller/SettingsWindow.xaml.cs
+++ Six Screens Controller/SettingsWindow.xaml.cs
@@ -23,36 +23,6 @@ namespace Six_Screens_Controller
             this.DataContext = config;
         }
 
-        private void serverBrowse_Click(object sender, RoutedEventArgs e)
-        {
-            OpenFileDialog openFileDialog = new OpenFileDialog();
-            if (openFileDialog.ShowDialog() == true)
-                server.Text = openFileDialog.FileName;
-
-        }
-
-        private void interpreterBrowse_Click(object sender, RoutedEventArgs e)
-        {
-            var dlg = new CommonOpenFileDialog();
-            dlg.Title = "Выбор расположения Python";
-            dlg.IsFolderPicker = true;
-            dlg.InitialDirectory = Directory.GetCurrentDirectory();
-
-            dlg.AddToMostRecentlyUsedList = false;
-            dlg.AllowNonFileSystemItems = false;
-            dlg.DefaultDirectory = Directory.GetCurrentDirectory();
-            dlg.EnsureFileExists = true;
-            dlg.EnsurePathExists = true;
-            dlg.EnsureReadOnly = false;
-            dlg.EnsureValidNames = true;
-            dlg.Multiselect = false;
-            dlg.ShowPlacesList = true;
-            if (dlg.ShowDialog() == CommonFileDialogResult.Ok)
-            {
-                interpreterPython.Text = dlg.FileName;
-            }
-        }
-
         private void Ok_Click(object sender, RoutedEventArgs e)
         {
             this.DialogResult = true;
