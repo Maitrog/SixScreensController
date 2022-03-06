86ece086b93a6ba8ededfa2029a70d0ab809bfd5
Maitrog <35650757+Maitrog@users.noreply.github.com>
2022-03-06 16:11:13 +0300

    Add creating config file if it doesn't exist

--- Six Screens Controller/App.xaml.cs
+++ Six Screens Controller/App.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using Newtonsoft.Json;
 using System;
-using System.Diagnostics;
 using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -9,13 +11,18 @@ namespace Six_Screens_Controller
 {
     public partial class App : Application
     {
-        public Config config = JsonConvert.DeserializeObject<Config>(File.ReadAllText("config.json"));
-        readonly Process process = new Process();
+        public Config config;
 
         private void App_Startup(object sender, StartupEventArgs e)
         {
             try
             {
+                if (!File.Exists("config.json"))
+                {
+                    CreateConfigFile();
+                }
+                config = JsonConvert.DeserializeObject<Config>(File.ReadAllText("config.json"));
+
                 if (!string.IsNullOrEmpty(config.Protocol) && !string.IsNullOrEmpty(config.Host) && !string.IsNullOrEmpty(config.Port))
                 {
                     string[] args = { "--urls", $"{config.Protocol}://{config.Host}:{config.Port}" };
@@ -29,6 +36,30 @@ namespace Six_Screens_Controller
             {
                 MessageBox.Show(ex.Message);
             }
+        }
+
+        private static void CreateConfigFile()
+        {
+            string localIP = GetLocalIPAddress();
+            Config config = new Config() { Protocol = "http", Host = localIP, Port = "5000", FirstStart = true };
+            Encoding utf8 = Encoding.UTF8;
+
+            using (FileStream fs = File.Create("config.json"))
+            {
+                fs.Write(utf8.GetBytes(JsonConvert.SerializeObject(config)));
+            }
+        }
+
+        private static string GetLocalIPAddress()
+        {
+            string localIP;
+            using (Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, 0))
+            {
+                socket.Connect("8.8.8.8", 65530);
+                IPEndPoint endPoint = socket.LocalEndPoint as IPEndPoint;
+                localIP = endPoint.Address.ToString();
+            }
+            return localIP;
         }
     }
 }
